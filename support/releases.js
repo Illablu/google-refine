7d85b32fa63a6c39f75276329ca5c0d23fab1ac6
dfhuynh <dfhuynh@7d457c2a-affb-35e4-300a-418c747d4874>
2010-05-09 20:40:12 +0000

    New release.

--- support/releases.js
+++ support/releases.js
@@ -1,7 +1,12 @@
 var GridworksReleases = [
-  {
-    "description": "Gridworks 1.0",
-    "version": "1.0",
-    "revision": "r655"
-  }
+    {
+        "description": "Gridworks 1.0",
+        "version": "1.0",
+        "revision": "r667"
+    },
+    {
+        "description": "Gridworks 1.0",
+        "version": "1.0",
+        "revision": "r655"
+    }
 ];
