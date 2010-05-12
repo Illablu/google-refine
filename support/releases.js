ba5717ac7335294dea8bc5588045ffa2ecb4e4c8
dfhuynh <dfhuynh@7d457c2a-affb-35e4-300a-418c747d4874>
2010-05-12 23:14:17 +0000

    Releasing 1.0.1-r732.

--- support/releases.js
+++ support/releases.js
@@ -1,5 +1,10 @@
 var GridworksReleases = [
     {
+      description: "Gridworks 1.0.1",
+      version: "1.0.1",
+      revision: "r732"
+    },
+    {
         "description": "Gridworks 1.0",
         "version": "1.0",
         "revision": "r667"
