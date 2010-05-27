0fc9f3598f6cb0f86eb99a4c989f19b299c900b3
dfhuynh <dfhuynh@7d457c2a-affb-35e4-300a-418c747d4874>
2010-05-27 23:50:47 +0000

    Release 1.1.

--- support/releases.js
+++ support/releases.js
@@ -2,6 +2,11 @@ var GridworksReleases = {
     "homepage" : "http://code.google.com/p/freebase-gridworks/wiki/Downloads",
     "releases" : [
         {
+            "description": "Gridworks 1.1",
+            "version": "1.1",
+            "revision": "r878"
+        },
+        {
             "description": "Gridworks 1.0.1",
             "version": "1.0.1",
             "revision": "r732"
