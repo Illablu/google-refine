392b77ba2836def628ffb551edb78863adef1374
tfmorris@gmail.com <tfmorris@gmail.com@7d457c2a-affb-35e4-300a-418c747d4874>
2013-07-27 19:45:16 +0000

    Revert to previous revision

--- support/releases.js
+++ support/releases.js
@@ -40,11 +40,3 @@ var GoogleRefineReleases = releases;
 // This is for back compatibility... remove this after Gridworks usage phases out
 var GridworksReleases = releases;
 
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-35838111-2', 'google-refine.googlecode.com');
-  ga('send', 'pageview');
-
